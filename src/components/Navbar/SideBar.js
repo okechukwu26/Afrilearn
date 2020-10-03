115565bd610962a8313e92081fb0588c794511cf
oke <priceblings@gmail.com>
2020-10-03 02:17:00 +0100

    init

--- src/components/Navbar/SideBar.js
+++ src/components/Navbar/SideBar.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 const token = localStorage.getItem('token');
 
